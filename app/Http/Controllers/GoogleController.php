6c3090126cee2348a297f5806debed164b759c81
naylaraisi <77318032+MFaisalQasim@users.noreply.github.com>
2022-03-19 21:37:04 +0500

    03 19 22

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+namespace App\Http\Controllers;
+
+  
+
+
+use Socialite;
+
+use Auth;
+
+use Exception;
+
+use App\User;
+
+class GoogleController extends Controller
+{
+    public function redirectToGoogle()
+
+    {
+        return Socialite::driver('google')->redirect();
+
+    }
+    public function handleGoogleCallback()
+
+    {
+
+        try {
+
+    
+
+            $user = Socialite::driver('google')->user();
+
+            
+            
+            $finduser = User::where('google_id', $user->id)->first();
+
+     
+
+            if($finduser){
+
+     
+
+                Auth::login($finduser);
+
+    
+
+                return redirect('/home');
+
+     
+
+            }else{
+
+                $newUser = User::create([
+
+                    'name' => $user->name,
+
+                    'email' => $user->email,
+
+                    'google_id'=> $user->id,
+
+                    'password' => encrypt('123456dummy')
+
+                ]);
+
+    
+
+                Auth::login($newUser);
+
+     
+
+                return redirect('/home');
+
+            }
+
+    
+
+        } catch (Exception $e) {
+
+            dd($e->getMessage());
+
+        }
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes();
+
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('auth/google', [App\Http\Controllers\GoogleController::class, 'redirectToGoogle']);
+
+Route::get('auth/google/callback', 'Auth\GoogleController@handleGoogleCallback');
